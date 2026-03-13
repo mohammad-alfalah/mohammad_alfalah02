aa9091fcb696b179b8c05ce865ae37432a088cd1
ibrahim alfalah <ibrahim.alfalah22@gmail.com>
2026-03-14 02:34:41 +0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mohammad_alfalah02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
